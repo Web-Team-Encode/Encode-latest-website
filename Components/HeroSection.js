4550e171d3a598a3343c664e361a8a4f56fa8d66
Aniket-Patel-swg <patelaniket1207@gmail.com>
2023-08-20 15:06:56 +0530

    Minor content change on website

--- Components/HeroSection.js
+++ Components/HeroSection.js
@@ -43,7 +43,7 @@ const HeroSection = () => {
                     <div className={styles.hero_upper_content}>
                         <section className={styles.inner_flex}>
                             <h1>
-                                Weird Mind&apos;s <br /> boundless opportunities
+                                Curious Mind&apos;s <br /> boundless opportunities
                             </h1>
                             <section className={styles.event_button}>
                                 <Link href="#tagline">Our Events
